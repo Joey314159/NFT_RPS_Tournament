8cdf7457d68f70dc94fc48739eccff30f9dd6855
Joel Alvarado <107654806+Joey314159@users.noreply.github.com>
2025-04-01 15:07:32 -0700

    2 key components
    In this commit I am adding files that we need in order to properly create the tournament and play the game as it should be done.

---
+++ Main.sol
@@ -0,0 +1,3 @@
+//In this file we will actually play the game
+//Creating instances of the classes/contracts we need in order to play it
+//In this class we will create the players and gift the token to the winner
---
+++ Tournament.sol
@@ -0,0 +1,2 @@
+//In this file we will create the functions needed to play the game
+//Such as randomize values, compare the values, and choose a winner 
